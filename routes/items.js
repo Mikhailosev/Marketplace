3b3a33b1f388445b956da7da25e198a852b0b878
mikhailosev <mikhail.losev2@gmail.com>
2019-04-25 19:50:09 +0300

    pre review changes

--- routes/items.js
+++ routes/items.js
@@ -30,7 +30,7 @@ const upload = multer({
 });
 
 const Item = require("../models/Item");
-const User=require('../models/User')
+const User = require("../models/User");
 
 const { ensureAuthenticated } = require("../config/auth");
 
@@ -39,11 +39,11 @@ router.get("/items", ensureAuthenticated, (req, res) => {
     user: req.user.name
   });
 });
-router.get('/search', (req, res) => {
-  Item.find({name: { $regex: req.query.searchField } }).then(result => {
-  res.json(result);
-console.log(result)
-});
+router.get("/search", (req, res) => {
+  Item.find({ name: { $regex: req.query.searchField } }).then(result => {
+    res.json(result);
+    console.log(result);
+  });
 });
 
 router.post(
@@ -65,7 +65,6 @@ router.post(
       .then(result => {
         console.log(result);
         res.json(result);
-
       })
       .catch(err => {
         console.log(err);
@@ -73,23 +72,36 @@ router.post(
           error: err
         });
       });
-   res.redirect('/dashboard/shop');}
+    res.redirect("/dashboard/shop");
+  }
 );
 router.get("/shopItem/:itemId", (req, res, next) => {
   const id = req.params.itemId;
   Item.findById(id)
     .select("name size quality description price itemImage userId")
-    .then(doc => {
-      if (doc) {
-        res.status(201).json({
-          message: "HAndling post request for items",
-          createdProduct: doc
-        });
-      } else {
-        res
-          .status(404)
-          .json({ message: "No valid entry found for the product" });
-      }
+  .then(doc => res.status(201).json(doc));
+});
+
+router.get("/shopItems", (req, res) => {
+  Item.find()
+    .select("name size quality description price itemImage")
+    .then(docs => {
+      res.send(docs);
+      //if(docs.length>=0){
+    });
+  //}else{
+  //  res.status(404).json({
+  //      message:'No entries found'
+  // });
+  //}
+});
+router.get("/passuser", ensureAuthenticated, (req, res, next) => {
+  const userId = req.user._id;
+  User.find({
+    _id: userId
+  })
+    .then(docs => {
+      res.send(docs);
     })
     .catch(err => {
       console.log(err);
@@ -98,75 +110,71 @@ router.get("/shopItem/:itemId", (req, res, next) => {
       });
     });
 });
-
-
-router.get("/shopItems", (req, res) => {
-  Item.find()
-    .select("name size quality description price itemImage")
-    .then(docs => {
-      res.send(docs)
-      //if(docs.length>=0){ 
-      });
-      //}else{
-      //  res.status(404).json({
-      //      message:'No entries found'
-      // });
-      //}
-});
-router.get('/passuser', ensureAuthenticated, (req,res,next)=>{
-  const userId=req.user._id
-  User.find({
-    _id: userId
-  })
-  .then(docs => {
-    res.send(docs)
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({
-      error: err
-    });
-  });
-})
-router.get("/shopItemsCreatedByUser",ensureAuthenticated, (req, res) => {
+router.get("/shopItemsCreatedByUser", ensureAuthenticated, (req, res) => {
   const userId = req.user._id;
   Item.find({
     userId: userId
   })
-  
+
     .select("name size quality description price itemImage")
     .then(docs => {
-      res.send(docs)
-      //if(docs.length>=0){ 
-      });
-      //}else{
-      //  res.status(404).json({
-      //      message:'No entries found'
-      // });
-      //}
+      res.send(docs);
+      //if(docs.length>=0){
+    });
+  //}else{
+  //  res.status(404).json({
+  //      message:'No entries found'
+  // });
+  //}
 });
-router.patch("/shopItem/:itemId", (req, res, next) => {
+router.post("/updatedItem/:itemId", ensureAuthenticated,  upload.single("itemImage"), (req, res, next) => {
   const id = req.params.itemId;
-  const updateOps = {};
-  for (const ops of req.body) {
-    updateOps[ops.propName] = ops.value;
-  }
-  Item.update({ _id: id }, { $set: { updateOps } });
+  Item.findOneAndUpdate({_id:id}, { 
+    name:req.body.name,
+    size: req.body.size,
+    quality: req.body.quality,
+    price: req.body.price,
+    description: req.body.description,
+    itemImage: req.file.path
+  } , {new: true},(error, item)=>{
+    if (error){
+      res.send(error)
+    }
+   res.redirect('/dashboard/shop');
+})
 });
 //delete
 router.delete("/delete/:itemId", (req, res, next) => {
   const id = req.params.itemId;
-  Item.findOneAndRemove({
+  Item.deleteOne({
     _id: id
-  }) /*.then(res=>{
-    res.status(200).json(result)
-  }).catch(err=>{
-    console.log(err)
-    res.status(500).json({
-
-    })*/;
+  }).then(docs => {
+    res.send(docs);
+    //if(docs.length>=0){
+  });
 });
-router.get('/shop', (req, res) => {
-res.render('shop');
+
+router.get("/updateItem/:itemId", (req, res) => {
+  const id = req.params.itemId;
+  Item.findById(id)
+    .select("name size quality description price itemImage userId")
+    .then(doc => {
+      res.send(doc);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+router.get("/shop", (req, res) => {
+  res.render("shop");
+});
+router.get("/update/:itemId", ensureAuthenticated, (req, res) => {
+  res.render("updateItem");
+});
+router.get("/singleItem/:itemId", (req, res) => {
+  res.render("singleItem");
 });
 module.exports = router;
